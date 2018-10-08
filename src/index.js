ecf3abad0f25172321e9ccc0ef7862ae2f3350da
liuhonglei <233624406@qq.com>
2018-10-08 15:56:30 +0800

    add app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM ,{render} from 'react-dom';
+import App from './app'
 
-ReactDOM.render(
-    <h1> Hello World </h1>,
-    document.getElementById('app')
-)
+//ReactDOM.render(
+    //<h1> Hello World </h1>,
+    //document.getElementById('app')
+//)
+render(<App />,document.getElementById('app'))
